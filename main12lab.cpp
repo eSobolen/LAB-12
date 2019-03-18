2dd71c854315982485a214c7a44742366de1de6c
eSobolen <43523040+eSobolen@users.noreply.github.com>
2019-03-19 01:57:16 +0300

    Add files via upload

---
+++ main12lab.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+int random(int R){ return rand()%R;}
+
+int main(){
+    int A[80],N,i,hash;
+    printf("enter N: ");
+    scanf("%d", &N);
+    printf("number     hash ");
+    for(i=0;i<N;i++){
+       A[i]=random(100)-49; 
+        //printf("%4.d",A[i] );
+       
+      if(A[i]>0){  
+        
+     printf("\n");
+    
+      
+     printf("%5d",A[i]);
+     hash=A[i]%10;   
+     
+     printf("%8d",hash);
+           }  }
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("       number          hash ");
+       for(i=0;i<N;i++){
+      // A[i]=random(100)-51; 
+         if(A[i]<0){  
+        
+     printf("\n");
+    
+      
+     printf("%10d",A[i]);
+     hash=A[i]%10;   
+     
+     printf("%18d",hash);
+           }  }
+}
